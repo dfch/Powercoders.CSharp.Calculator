7a1affd987c9a441c6d7580f4ada41ca121224f5
Ronald Rink <ronald.rink@d-fens.net>
2017-03-23 09:52:21 +0100

    Initial commit
    * added extracted source

---
+++ src/Powercoders.Calculator.Brain/PcCalcBigBrain.cs
@@ -0,0 +1,35 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="PcCalcBigBrain.cs" company="Genossenschaft Migros Aare">
+//   M. Styczynski
+// </copyright>
+// <summary>
+//   Main class for Calculator UI
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Powercoders.Calculator.Brain
+{
+    /// <summary>
+    /// BigBrain implemented as an Singleton
+    /// </summary>
+    public sealed class PcCalcBigBrain
+    {
+        /// <summary>
+        /// The instance var of the class
+        /// </summary>
+        private static readonly PcCalcBigBrain StaticInstance = new PcCalcBigBrain();
+
+        /// <summary>
+        /// Prevents a default instance of the <see cref="PcCalcBigBrain"/> class from being created.
+        /// </summary>
+        private PcCalcBigBrain()
+        {
+            // TODO: class initializations has to be here...
+        }
+
+        /// <summary>
+        /// Returns an instance of the class
+        /// </summary>
+        public static PcCalcBigBrain Instance => StaticInstance;
+    }
+}
---
+++ src/Powercoders.Calculator.UI/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="MainWindow.xaml.cs" company="Genossenschaft Migros Aare">
+//   M. Styczynski
+// </copyright>
+// <summary>
+//   Calculator UI main window
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Powercoders.Calculator.UI
+{
+    using System;
+    using System.Windows;
+
+    /// <summary>
+    ///     Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="MainWindow" /> class.
+        /// </summary>
+        public MainWindow()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// The digit clicked.
+        /// </summary>
+        /// <param name="sender">
+        /// The sender.
+        /// </param>
+        /// <param name="e">
+        /// The e.
+        /// </param>
+        /// <exception cref="NotImplementedException">
+        /// </exception>
+        private void DigitClicked(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// The func clicked.
+        /// </summary>
+        /// <param name="sender">
+        /// The sender.
+        /// </param>
+        /// <param name="e">
+        /// The e.
+        /// </param>
+        /// <exception cref="NotImplementedException">
+        /// </exception>
+        private void FuncClicked(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// The helper clicked.
+        /// </summary>
+        /// <param name="sender">
+        /// The sender.
+        /// </param>
+        /// <param name="e">
+        /// The e.
+        /// </param>
+        /// <exception cref="NotImplementedException">
+        /// </exception>
+        private void HelperClicked(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
